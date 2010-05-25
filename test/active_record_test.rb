da0019fcea6162b7901af5cb28cd66c46b635d03
Nick Ragaz <nick.ragaz@gmail.com>
2010-05-24 21:02:26 -0400

    add fixture

--- test/active_record_test.rb
+++ test/active_record_test.rb
@@ -3,8 +3,12 @@ require 'test_helper'
 class ActiveRecordTest < ActiveSupport::TestCase
   load_schema
 
-  test "create user without exception" do
-    assert_nothing_raised { User.new(:name => 'John Doe') }
+  test "create and save user without exception" do
+    assert_nothing_raised {
+      @user = User.new(:name => 'John Doe', :avatar => File.open("test/fixtures/kitten.jpg"))
+      @user.save!
+    }
+    @user.destroy
   end
   
 end
--- test/active_resource_test.rb
+++ test/active_resource_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ActiveResourceTest < ActiveSupport::TestCase
   load_schema
 
-  test "create user resource without exception" do
+  test "build user resource without exception" do
     assert_nothing_raised { Api::User.new(:name => 'John Doe') }
   end
 end
